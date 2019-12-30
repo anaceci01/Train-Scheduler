624452c9c743b9065ebf548865a62292b3cbf5e5
Ana Davis <anac.davis@gmail.com>
2019-12-29 23:36:48 -0700

    code to retrieve the most recent user

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-var url = "";
+var url = "https://my-train-schedule.firebaseio.com/";
 var dataRef = new Firebase(url);
 var name = '';
 var destination = '';
+var fistTrainTime = '';
 var frequeny = '';
 var nextTrain = '';
 var nextTrainFormatted = '';
@@ -13,3 +14,14 @@ var tRemainder = '';
 var minutesTillTrain = '';
 var keyHolder = '';
 var getKey = '';
+
+
+$(document).ready(function() {
+    $("#add-train").on("clik", function() {
+        name = $('#name-input').val().trim();
+        destination = $('#destination-input').val().trim();
+        frequency = $('#frequency-input').val().trim();
+        firstTimeConverted = moment(firstTrainTime, "hh:mm").substract(1, "years");
+        console.log(name)
+    })
+})
