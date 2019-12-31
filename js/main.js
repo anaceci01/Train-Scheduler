74d26824d60defa3e75334656ac9df51c9b4319c
Ana Davis <anac.davis@gmail.com>
2019-12-31 00:35:29 -0700

    firebase variable added, still working on ref

--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
-  var firebaseConfig = {
+  var config = {
       apiKey: "AIzaSyBbsx4AoHpHW19pzc_3uY5nIBeK6tF8g8A",
       authDomain: "train-tracker-534f3.firebaseapp.com",
       databaseURL: "https://train-tracker-534f3.firebaseio.com",
       projectId: "train-tracker-534f3",
       storageBucket: "train-tracker-534f3.appspot.com",
-      messagingSenderId: "157494367628",
-      appId: "1:157494367628:web:92b4e7a2df765ee071b2a9"
   };
 
-  firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp(config);
 
   var url = "https://train-tracker-534f3.firebaseio.com",
       var dataRef = new Firebase(url);
