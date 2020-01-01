66866bb784cebc4754075a51b1679876dcd91792
Ana Davis <anac.davis@gmail.com>
2020-01-01 15:19:32 -0700

    form will be cleared after function is called when user enters new train info

--- js/main.js
+++ js/main.js
@@ -8,51 +8,95 @@
 
   firebase.initializeApp(config);
 
-  var url = "https://train-tracker-534f3.firebaseio.com",
-      var dataRef = new Firebase(url);
-  var name = '';
-  var destination = '';
-  var fistTrainTime = '';
-  var frequeny = '';
-  var nextTrain = '';
-  var nextTrainFormatted = '';
-  var minutesAway = '';
-  var firstTimeConverted = '';
-  var currentTime = '';
-  var diffTime = '';
-  var tRemainder = '';
-  var minutesTillTrain = '';
-  var keyHolder = '';
-  var getKey = '';
+  //on click function to add trains
+  var trainData = firebase.database();
+  $("#add-train").on("click", function(event) {
+      event.noDefault();
+
+      //user input
+      var name = $('#name-input').val().trim();
+      var destination = $('#destination-input').val().trim();
+      var frequency = $('#frequency-input').val().trim();
+      var firstTrain = moment(firstTrainTime, "hh:mm").substract(1, "years");
+
+      var newTrain = {
+          name: name,
+          destination: destination,
+          frequency: frequency,
+          firstTrain: firstTrain
+      };
+
+      trainData.ref().push(newTrain);
+
+      console.log(newTrain.name);
+      console.log(newTrain.destination);
+      console.log(newTrain.frequency);
+      console.log(newTrain.firstTrain);
+
+      //form clears tex
+      $("#train-name-input").val("");
+      $("#destination-input").val("");
+      $("#first-train-input").val("");
+      $("#frequency-input").val("");
+  });
+
+  //create a new row when user adds train
+  trainData.re().on("child_added"),
+      function(childSnapshot, prevChildKey) {
+          console.log(childSnapshot.val());
+      }
 
 
-  $(document).ready(function() {
-      $("#add-train").on("clik", function() {
-          name = $('#name-input').val().trim();
-          destination = $('#destination-input').val().trim();
-          frequency = $('#frequency-input').val().trim();
-          firstTimeConverted = moment(firstTrainTime, "hh:mm").substract(1, "years");
 
-          //push code
-          keyHolder = dataRef.push({
-              name: name,
-              destination: destination,
-              fistTrainTime: firstTrainTime,
-              frequency: frequeny,
-              nextTrainFormatted: nextTrainFormatted,
-              minutesTillTrain: minutesTillTrain
-          });
 
-          console.log(keyHolder.path.u[0]);
-          var key = keyHolder.path.u[0];
-          console.log(key);
 
-          $('#name-input').val('');
-          $('#destination-input').val('');
-          $('#first-train-time-input').val('');
-          $('#frequency-input').val('');
 
-          return false;
-      });
 
-  })
+
+  //   var url = "https://train-tracker-534f3.firebaseio.com";
+  //   var name = '';
+  //   var destination = '';
+  //   var fistTrainTime = '';
+  //   var frequeny = '';
+  //   var nextTrain = '';
+  //   var nextTrainFormatted = '';
+  //   var minutesAway = '';
+  //   var firstTimeConverted = '';
+  //   var currentTime = '';
+  //   var diffTime = '';
+  //   var tRemainder = '';
+  //   var minutesTillTrain = '';
+  //   var keyHolder = '';
+  //   var getKey = '';
+
+
+  //   $(document).ready(function() {
+  //       $("#add-train").on("clik", function() {
+  //           name = $('#name-input').val().trim();
+  //           destination = $('#destination-input').val().trim();
+  //           frequency = $('#frequency-input').val().trim();
+  //           firstTimeConverted = moment(firstTrainTime, "hh:mm").substract(1, "years");
+
+  //push code
+  keyHolder = dataRef.push({
+      name: name,
+      destination: destination,
+      fistTrainTime: firstTrainTime,
+      frequency: frequeny,
+      nextTrainFormatted: nextTrainFormatted,
+      minutesTillTrain: minutesTillTrain
+  });
+
+  //           console.log(keyHolder.path.u[0]);
+  //           var key = keyHolder.path.u[0];
+  //           console.log(key);
+
+  //           $('#name-input').val('');
+  //           $('#destination-input').val('');
+  //           $('#first-train-time-input').val('');
+  //           $('#frequency-input').val('');
+
+  //           return false;
+  //       });
+
+  //   })
